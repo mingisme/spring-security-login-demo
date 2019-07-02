984d22cd2b14ee20b50d2283c92e478ccce38b8d
swang <mingisme@126.com>
2019-07-02 11:13:46 +0800

    fix error

--- src/main/java/com/swang/secureapp/AppSecurityConfig.java
+++ src/main/java/com/swang/secureapp/AppSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.swang.secureapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -16,38 +15,38 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Autowired
-//    private UserDetailsService userDetailService;
-//
-//    @Bean
-//    public AuthenticationProvider authProvider() {
-//
-//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-//        daoAuthenticationProvider.setUserDetailsService(userDetailService);
-//        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
-//        return daoAuthenticationProvider;
-//    }
+    @Autowired
+    private UserDetailsService userDetailService;
+
+    @Bean
+    public AuthenticationProvider authProvider() {
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailService);
+        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return daoAuthenticationProvider;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests().antMatchers("/login").permitAll()
-                .anyRequest().authenticated();
-
 //        http.csrf().disable()
-//                .authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login").permitAll()
-//                .and()
-//                .logout().invalidateHttpSession(true)
-//                .clearAuthentication(true)
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                .logoutSuccessUrl("/logout-success").permitAll();
+//                .authorizeRequests().antMatchers("/login").permitAll()
+//                .anyRequest().authenticated();
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout-success").permitAll();
     }
 
     //    @Bean
